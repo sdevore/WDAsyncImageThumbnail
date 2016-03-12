2068a5821681c6a3435bf67d5bc686df6e94e484
sdevore <sdevore@gmail.com>
2016-03-11 17:58:03 -0700

    Add support for using some configuration settings

--- Pod/Classes/WDAsyncImageThumbnail.m
+++ Pod/Classes/WDAsyncImageThumbnail.m
@@ -17,15 +17,14 @@
 - (BOOL)WD_handleLoadCancellation:(id)syncObj;
 @end
 
-@implementation WDAsyncImageThumbnail{
+@implementation WDAsyncImageThumbnail {
     BOOL isCancelled;
 }
 
 #pragma mark - Initialization and backend
 
-+ (void)initialize{
-
-    if( self == [WDAsyncImageThumbnail class] ){
++ (void)initialize {
+    if (self == [WDAsyncImageThumbnail class]) {
         [[self class] setDispatchGroup:[[self class] defaultDispatchGroup]];
         [[self class] initDispatchSemaphore];
     }
@@ -34,25 +33,23 @@
 static dispatch_group_t dispatchGroup;
 static dispatch_semaphore_t loadSemaphore;
 
-+ (void)initDispatchSemaphore{
-
-    @synchronized(self){
++ (void)initDispatchSemaphore {
+    @synchronized(self) {
         static dispatch_once_t dOnceTok;
+        NSLog(@"wdCollectionLoadingThreads: %d",wdCollectionLoadingThreads);
         dispatch_once(&dOnceTok, ^{
             loadSemaphore = dispatch_semaphore_create(wdCollectionLoadingThreads);
         });
     }
 }
 
-+ (dispatch_semaphore_t)dispatchSemaphore{
-
-    @synchronized(self){
++ (dispatch_semaphore_t)dispatchSemaphore {
+    @synchronized(self) {
         return loadSemaphore;
     }
 }
 
-+ (dispatch_group_t)defaultDispatchGroup{
-
++ (dispatch_group_t)defaultDispatchGroup {
     static dispatch_once_t dOnceToken;
     static dispatch_group_t defaultGroup;
     dispatch_once(&dOnceToken, ^{
@@ -62,24 +59,21 @@ static dispatch_semaphore_t loadSemaphore;
     return defaultGroup;
 }
 
-+ (void)setDispatchGroup:(dispatch_group_t)aDispatchGroup{
-
-    @synchronized(self){
++ (void)setDispatchGroup:(dispatch_group_t)aDispatchGroup {
+    @synchronized(self) {
         dispatchGroup = aDispatchGroup;
     }
 }
 
-+ (dispatch_group_t)dispatchGroup{
-
-    @synchronized(self){
++ (dispatch_group_t)dispatchGroup {
+    @synchronized(self) {
         return dispatchGroup;
     }
 }
 
-- (instancetype)initWithImageCache:(NSCache *)imageCache imageURL:(NSURL *)imageURL{
-
+- (instancetype)initWithImageCache:(NSCache *)imageCache imageURL:(NSURL *)imageURL {
     self = [super init];
-    if( self ){
+    if (self) {
         _imageCache = imageCache;
         _imageURL = imageURL;
         _imageState = WD_TLI_IDLE;
@@ -89,179 +83,207 @@ static dispatch_semaphore_t loadSemaphore;
     return self;
 }
 
-+ (instancetype)imageWithImageCache:(NSCache *)imageCache imageURL:(NSURL *)imageURL{
-
++ (instancetype)imageWithImageCache:(NSCache *)imageCache imageURL:(NSURL *)imageURL {
     return [[self alloc] initWithImageCache:imageCache imageURL:imageURL];
 }
 
 #pragma mark - Interface
 
-- (void)cancelLoad{
-
+- (void)cancelLoad {
     WDThreadLoadedImageState state;
-    @synchronized(self){
+    @synchronized(self) {
         state = _imageState;
         isCancelled = YES;
     }
+#if wdDebugLoggingEnabled
     NSLog(@"Image order to cancell. Image state during cancel was: %lu", state);
+#endif
 }
 
-- (void)releaseImage{
-
-    @synchronized(self){
+- (void)releaseImage {
+    @synchronized(self) {
         CGImageRef im;
-        if( (im = [self WD_getImageFromCacheSelf:self]) ){
+        if ((im = [self WD_getImageFromCacheSelf:self])) {
             [self.imageCache removeObjectForKey:[self.imageURL absoluteString]];
             CGImageRelease(im);
         }
     }
 }
 
-- (void)loadImageWithCallbackBlock:(void (^)(CGImageRef aImageRef, NSError *aError))aBlock{
-
-    @synchronized(self){
-        switch(_imageState){
-            case WD_TLI_IDLE:{
-                isCancelled = NO;
-                CGImageRef pCGImage = [self WD_getImageFromCacheSelf:self];
-                if( pCGImage ){
-                    NSLog(@"Image already in cache. No need to load.url=%@", [self.imageURL lastPathComponent]);
-                    _imageState = WD_TLI_LOAD_COMPLETED;
-                    aBlock(pCGImage, nil);
-                    return;
-                }
-
-                NSLog(@"scheduling a load.pic=%@", [self.imageURL lastPathComponent]);
-                _imageState = WD_TLI_LOAD_SCHEDULED;
-                [self WD_scheduleAsyncLoadWithCompletionBlock:aBlock];
-                break;
+- (void)loadImageWithCallbackBlock:(void (^)(CGImageRef aImageRef, NSError *aError))aBlock {
+    @synchronized(self) {
+        switch (_imageState) {
+        case WD_TLI_IDLE: {
+            isCancelled = NO;
+            CGImageRef pCGImage = [self WD_getImageFromCacheSelf:self];
+            if (pCGImage) {
+#if wdDebugLoggingEnabled
+                NSLog(@"Image already in cache. No need to load.url=%@", [self.imageURL lastPathComponent]);
+#endif
+                _imageState = WD_TLI_LOAD_COMPLETED;
+                aBlock(pCGImage, nil);
+                return;
             }
-            case WD_TLI_LOAD_COMPLETED:{
-                NSLog(@"Load complete. Getting image from cache and calling back.");
-                CGImageRef pImage = [self WD_getImageFromCacheSelf:self];
-                aBlock(pImage, nil);
-                break;
-            }
-            case WD_TLI_LOAD_SCHEDULED:{
-                NSLog(@"Load is already scheduled. Leaving.");
-                break;
-            }
-            default:{
-                NSLog(@"Unknown state. Will raise an exception.");
-                [NSException raise:@"unknown state" format:@"unknown state"];
-            }
+#if wdDebugLoggingEnabled
+            NSLog(@"scheduling a load.pic=%@", [self.imageURL lastPathComponent]);
+#endif
+            _imageState = WD_TLI_LOAD_SCHEDULED;
+            [self WD_scheduleAsyncLoadWithCompletionBlock:aBlock];
+            break;
+        }
+        case WD_TLI_LOAD_COMPLETED: {
+#if wdDebugLoggingEnabled
+            NSLog(@"Load complete. Getting image from cache and calling back.");
+#endif
+            CGImageRef pImage = [self WD_getImageFromCacheSelf:self];
+            aBlock(pImage, nil);
+            break;
+        }
+        case WD_TLI_LOAD_SCHEDULED: {
+#if wdDebugLoggingEnabled
+            NSLog(@"Load is already scheduled. Leaving.");
+#endif
+            break;
+        }
+        default: {
+#if wdDebugLoggingEnabled
+            NSLog(@"Unknown state. Will raise an exception.");
+#endif
+            [NSException raise:@"unknown state" format:@"unknown state"];
+        }
         }
     }
 }
 
 #pragma mark - Private methods
 
-- (void)WD_cacheImage:(CGImageRef)aImage self:(WDAsyncImageThumbnail *)aSelf{
-
-//    [aSelf.imageCache setObject:CFBridgingRelease(aImage) forKey:[aSelf.imageURL absoluteString]];
-    [aSelf.imageCache setObject:(__bridge id) aImage forKey:[aSelf.imageURL absoluteString]];
+- (void)WD_cacheImage:(CGImageRef)aImage self:(WDAsyncImageThumbnail *)aSelf {
+    //    [aSelf.imageCache setObject:CFBridgingRelease(aImage) forKey:[aSelf.imageURL absoluteString]];
+    [aSelf.imageCache setObject:(__bridge id)aImage forKey:[aSelf.imageURL absoluteString]];
 }
 
-- (CGImageRef)WD_getImageFromCacheSelf:(WDAsyncImageThumbnail *)aSelf{
-
-    return (__bridge CGImageRef) [aSelf.imageCache objectForKey:[aSelf.imageURL absoluteString]];
+- (CGImageRef)WD_getImageFromCacheSelf:(WDAsyncImageThumbnail *)aSelf {
+    return (__bridge CGImageRef)[aSelf.imageCache objectForKey:[aSelf.imageURL absoluteString]];
 }
 
 static int64_t testC = 0;
 
-- (void)WD_scheduleAsyncLoadWithCompletionBlock:(void (^)(CGImageRef aImageRef, NSError *aError))aBlock{
-
+- (void)WD_scheduleAsyncLoadWithCompletionBlock:(void (^)(CGImageRef aImageRef, NSError *aError))aBlock {
     __weak WDAsyncImageThumbnail *weakSelf = self;
-    dispatch_group_async([[self class] dispatchGroup], dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)
-            , ^{
+    dispatch_group_async([[self class] dispatchGroup], dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
 
-                if( !weakSelf ){
-                    NSLog(@"Seems that the photo object has been deallocated before load started. Leaving..");
-                    //fuck the status, object already dead
-                    return;
-                }
+        if (!weakSelf) {
+#if wdDebugLoggingEnabled
+            NSLog(@"Seems that the photo object has been deallocated before load started. Leaving..");
+#endif
+            // fuck the status, object already dead
+            return;
+        }
 
-                WDAsyncImageThumbnail *strongSelf = weakSelf;
-                if( [strongSelf WD_handleLoadCancellation:strongSelf] ){return;}
+        WDAsyncImageThumbnail *strongSelf = weakSelf;
+        if ([strongSelf WD_handleLoadCancellation:strongSelf]) {
+            return;
+        }
 
-                CGImageRef pic = NULL;
-                NSError *error;
-                dispatch_semaphore_t dSema = [[strongSelf class] dispatchSemaphore];
-                dispatch_semaphore_wait(dSema, DISPATCH_TIME_FOREVER);
-                OSAtomicIncrement64Barrier(&testC);
-                NSLog(@"load counter start=%lld", testC);
-                @try{
-                    if( [strongSelf WD_handleLoadCancellation:strongSelf] ){return;}
+        CGImageRef pic = NULL;
+        NSError *error;
+        dispatch_semaphore_t dSema = [[strongSelf class] dispatchSemaphore];
+        dispatch_semaphore_wait(dSema, DISPATCH_TIME_FOREVER);
+        OSAtomicIncrement64Barrier(&testC);
+#if wdDebugLoggingEnabled
+        NSLog(@"load counter start=%lld", testC);
+#endif
+        @try {
+            if ([strongSelf WD_handleLoadCancellation:strongSelf]) {
+                return;
+            }
 
-                    __block BOOL continueLoad = YES;
-                    if( strongSelf.delegate
-                        && [strongSelf.delegate respondsToSelector:@selector(imageWillLoad:)] ){
-                        dispatch_sync(dispatch_get_main_queue(), ^{
-                            continueLoad = [strongSelf.delegate imageWillLoad:strongSelf];
-                        });
+            __block BOOL continueLoad = YES;
+            if (strongSelf.delegate && [strongSelf.delegate respondsToSelector:@selector(imageWillLoad:)]) {
+                dispatch_sync(dispatch_get_main_queue(), ^{
+                    continueLoad = [strongSelf.delegate imageWillLoad:strongSelf];
+                });
+            }
+            @synchronized(strongSelf) {
+                if (!continueLoad) {
+#if wdDebugLoggingEnabled
+                    NSLog(@"delegate said to stop the load. Cancelling..");
+#endif
+                    isCancelled = YES;
+                    if ([strongSelf WD_handleLoadCancellation:strongSelf]) {
+                        return;
                     }
-                    @synchronized(strongSelf){
-                        if( !continueLoad ){
-                            NSLog(@"delagate said to stop the load. Cancelling..");
-                            isCancelled = YES;
-                            if( [strongSelf WD_handleLoadCancellation:strongSelf] ){return;}
-                        }
-                    }
-
-                    NSLog(@"actual load will start for pic=%@", [strongSelf.imageURL lastPathComponent]);
-                    if( [WDThumbnailDataUtils isFilePhoto:strongSelf.imageURL] ){
-                        NSError *imageLoadErr;
-                        pic = [WDThumbnailDataUtils newThumbnailForImage:strongSelf.imageURL
-                                                    heigth:wdCollectionThumbnailMaxSize
-                                                    error:&imageLoadErr];
-                        if( imageLoadErr ) error = imageLoadErr;
-                    }else if( [WDThumbnailDataUtils isFileVideo:strongSelf.imageURL] ){
-                        NSError *videoLoadErr;
-                        pic = [WDThumbnailDataUtils newThumbnailForVideo:strongSelf.imageURL error:&videoLoadErr];
-                        if( videoLoadErr ) error = videoLoadErr;
-                    }else{
-                        NSLog(@"File neither photo nor video. File url=%@", strongSelf.imageURL);
-                        error = [NSError errorWithDomain:WD_ASYNC_IMAGE_ERROR_DOMAIN code:-1 userInfo:nil];
-                    }
-                }@finally{
-                    dispatch_semaphore_signal(dSema);
-                    NSLog(@"actual load did finish for pic=%@", [strongSelf.imageURL lastPathComponent]);
-                    OSAtomicDecrement64Barrier(&testC);
-                    NSLog(@"load counter finish=%lld", testC);
                 }
+            }
+#if wdDebugLoggingEnabled
+            NSLog(@"actual load will start for pic=%@", [strongSelf.imageURL lastPathComponent]);
+#endif
+            if ([WDThumbnailDataUtils isFilePhoto:strongSelf.imageURL]) {
+                NSError *imageLoadErr;
+                pic = [WDThumbnailDataUtils newThumbnailForImage:strongSelf.imageURL
+                                                          heigth:wdCollectionThumbnailMaxSize
+                                                           error:&imageLoadErr];
+                if (imageLoadErr)
+                    error = imageLoadErr;
+            } else if ([WDThumbnailDataUtils isFileVideo:strongSelf.imageURL]) {
+                NSError *videoLoadErr;
+                pic = [WDThumbnailDataUtils newThumbnailForVideo:strongSelf.imageURL error:&videoLoadErr];
+                if (videoLoadErr)
+                    error = videoLoadErr;
+            } else {
+#if wdDebugLoggingEnabled
+                NSLog(@"File neither photo nor video. File url=%@", strongSelf.imageURL);
+#endif
+                error = [NSError errorWithDomain:WD_ASYNC_IMAGE_ERROR_DOMAIN code:-1 userInfo:nil];
+            }
+        } @finally {
+            dispatch_semaphore_signal(dSema);
+#if wdDebugLoggingEnabled
+            NSLog(@"actual load did finish for pic=%@", [strongSelf.imageURL lastPathComponent]);
+#endif
+            OSAtomicDecrement64Barrier(&testC);
+#if wdDebugLoggingEnabled
+            NSLog(@"load counter finish=%lld", testC);
+#endif
+        }
 
-                if( !pic && !error ){
-                    NSAssert(NO, @"Fucked up!");
-                }
+        if (!pic && !error) {
+            NSAssert(NO, @"No picture and no errors!");
+        }
 
-                if( pic ){
-                    [strongSelf WD_cacheImage:pic self:strongSelf];
-                    NSLog(@"Image cached");
-                }
+        if (pic) {
+            [strongSelf WD_cacheImage:pic self:strongSelf];
+#if wdDebugLoggingEnabled
+            NSLog(@"Image cached");
+#endif
+        }
 
-                if( [strongSelf WD_handleLoadCancellation:strongSelf] ){return;}
-                _imageState = WD_TLI_LOAD_COMPLETED;
+        if ([strongSelf WD_handleLoadCancellation:strongSelf]) {
+            return;
+        }
+        _imageState = WD_TLI_LOAD_COMPLETED;
 
-                [strongSelf WD_runCallbackBlock:aBlock syncObject:strongSelf picture:pic error:error];
-            });
+        [strongSelf WD_runCallbackBlock:aBlock syncObject:strongSelf picture:pic error:error];
+    });
 }
 
 - (void)WD_runCallbackBlock:(void (^)(CGImageRef aImageRef, NSError *aError))aBlock
                  syncObject:(id)aSyncObj
                     picture:(CGImageRef)aPic
-                      error:(NSError *)aErr{
-
-    @synchronized(aSyncObj){
-        if( [NSThread isMainThread] ){
+                      error:(NSError *)aErr {
+    @synchronized(aSyncObj) {
+        if ([NSThread isMainThread]) {
             aBlock(aPic, aErr);
-        }else{
+        } else {
             dispatch_async(dispatch_get_main_queue(), ^{
-                @synchronized(aSyncObj){
-                    if( !isCancelled ){
+                @synchronized(aSyncObj) {
+                    if (!isCancelled) {
                         aBlock(aPic, aErr);
-                    }else{
-                        NSLog(@"Image load cancelled when block was waiting to run actual callback.Url=%@"
-                                , [((WDAsyncImageThumbnail *) aSyncObj).imageURL lastPathComponent]);
+                    } else {
+#if wdDebugLoggingEnabled
+                        NSLog(@"Image load cancelled when block was waiting to run actual callback.Url=%@",
+                              [((WDAsyncImageThumbnail *)aSyncObj).imageURL lastPathComponent]);
+#endif
                     }
                 }
             });
@@ -269,13 +291,13 @@ static int64_t testC = 0;
     }
 }
 
-- (BOOL)WD_handleLoadCancellation:(id)syncObj{
-
-    @synchronized(syncObj){
+- (BOOL)WD_handleLoadCancellation:(id)syncObj {
+    @synchronized(syncObj) {
         BOOL initIsCancelled = isCancelled;
-        if( isCancelled ){
-            NSLog(@"image load cancelled.Url=%@"
-                    , [((WDAsyncImageThumbnail *) syncObj).imageURL lastPathComponent]);
+        if (isCancelled) {
+#if wdDebugLoggingEnabled
+            NSLog(@"image load cancelled.Url=%@", [((WDAsyncImageThumbnail *)syncObj).imageURL lastPathComponent]);
+#endif
             isCancelled = NO;
             _imageState = WD_TLI_IDLE;
         }
@@ -283,9 +305,10 @@ static int64_t testC = 0;
     }
 }
 
-- (void)dealloc{
-
+- (void)dealloc {
+#if wdDebugLoggingEnabled
     NSLog(@"deallocating image: %@", [self.imageURL lastPathComponent]);
+#endif
 }
 
 @end
--- Pod/Classes/WDThumbnailDataUtils.m
+++ Pod/Classes/WDThumbnailDataUtils.m
@@ -17,44 +17,37 @@
 
 #pragma mark UTI file types analysis
 
-+ (CFStringRef)getFileUTIType:(NSURL *)url{
-
-    return (__bridge CFStringRef) ([[NSWorkspace sharedWorkspace] typeOfFile:[url path] error:nil]);
++ (CFStringRef)getFileUTIType:(NSURL *)url {
+    return (__bridge CFStringRef)([[NSWorkspace sharedWorkspace] typeOfFile:[url path] error:nil]);
 }
 
-+ (BOOL)isImageFileUTIBased:(CFStringRef)uti{
-
++ (BOOL)isImageFileUTIBased:(CFStringRef)uti {
     return [self doesUti:uti conformToUtiType:kUTTypeImage];
 }
 
-+ (BOOL)doesUti:(CFStringRef)aUti conformToUtiType:(CFStringRef)aUtiType{
-
-    if( aUti ){
++ (BOOL)doesUti:(CFStringRef)aUti conformToUtiType:(CFStringRef)aUtiType {
+    if (aUti) {
         return UTTypeConformsTo(aUti, aUtiType);
-    }else{
+    } else {
         return false;
     }
 }
 
-+ (BOOL)isVideoFileUTIBased:(CFStringRef)uti{
-
++ (BOOL)isVideoFileUTIBased:(CFStringRef)uti {
     return [self doesUti:uti conformToUtiType:kUTTypeMovie];
 }
 
-+ (BOOL)isFileVideo:(NSURL *)url{
-
++ (BOOL)isFileVideo:(NSURL *)url {
     return [self isVideoFileUTIBased:[self getFileUTIType:url]];
 }
 
-+ (BOOL)isFilePhoto:(NSURL *)aUrl{
-
++ (BOOL)isFilePhoto:(NSURL *)aUrl {
     return [self isImageFileUTIBased:[self getFileUTIType:aUrl]];
 }
 
 #pragma mark - Images
 
-+ (CGImageRef)newThumbnailForVideo:(NSURL *)videoPath error:(NSError *__autoreleasing *)aError{
-
++ (CGImageRef)newThumbnailForVideo:(NSURL *)videoPath error:(NSError *__autoreleasing *)aError {
     CGImageRef videoThumbnail;
     [videoPath startAccessingSecurityScopedResource];
     AVAsset *vid = [AVAsset assetWithURL:videoPath];
@@ -62,9 +55,11 @@
     CMTime thumbMoment = CMTimeMultiplyByFloat64(vDuration, 0.5f);
     AVAssetImageGenerator *imGen = [AVAssetImageGenerator assetImageGeneratorWithAsset:vid];
     videoThumbnail = [imGen copyCGImageAtTime:thumbMoment actualTime:NULL error:NULL];
-    if( videoThumbnail == NULL ){
+    if (videoThumbnail == NULL) {
+#if wdDebugLoggingEnabled
         NSLog(@"Unable to create video thumbnail");
-        if( aError != NULL ){
+#endif
+        if (aError != NULL) {
             *aError = [NSError errorWithDomain:WD_ASYNC_IMAGE_ERROR_DOMAIN code:-42 userInfo:nil];
         }
         return NULL;
@@ -73,8 +68,7 @@
     return videoThumbnail;
 }
 
-+ (CGImageRef)newThumbnailForImage:(NSURL *)imagePath heigth:(int)h error:(NSError *__autoreleasing *)aError{
-
++ (CGImageRef)newThumbnailForImage:(NSURL *)imagePath heigth:(int)h error:(NSError *__autoreleasing *)aError {
     CGImageSourceRef myImageSource;
     CFDictionaryRef myOptions = NULL;
     CFStringRef myKeys[2];
@@ -88,37 +82,40 @@
 
 
     myKeys[0] = kCGImageSourceShouldCache;
-    myValues[0] = (CFTypeRef) kCFBooleanTrue;
+    myValues[0] = (CFTypeRef)kCFBooleanTrue;
     myKeys[1] = kCGImageSourceShouldAllowFloat;
-    myValues[1] = (CFTypeRef) kCFBooleanTrue;
+    myValues[1] = (CFTypeRef)kCFBooleanTrue;
 
     [imagePath startAccessingSecurityScopedResource];
-    myOptions = CFDictionaryCreate(NULL, (const void **) myKeys, (const void **) myValues, 2
-            , &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
-    myImageSource = CGImageSourceCreateWithURL((__bridge CFURLRef) imagePath, myOptions);
+    myOptions = CFDictionaryCreate(NULL, (const void **)myKeys, (const void **)myValues, 2,
+                                   &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    myImageSource = CGImageSourceCreateWithURL((__bridge CFURLRef)imagePath, myOptions);
     CFRelease(myOptions);
 
-    if( myImageSource == NULL ){
+    if (myImageSource == NULL) {
+#if wdDebugLoggingEnabled
         NSLog(@"Image source is NULL.");
-        if( aError != NULL ) *aError = [NSError errorWithDomain:WD_ASYNC_IMAGE_ERROR_DOMAIN code:-40 userInfo:nil];
+#endif
+        if (aError != NULL)
+            *aError = [NSError errorWithDomain:WD_ASYNC_IMAGE_ERROR_DOMAIN code:-40 userInfo:nil];
         return NULL;
     }
 
     thumbnailSize = CFNumberCreate(NULL, kCFNumberIntType, &h);
     // Set up the thumbnail options.
     myKeysT[0] = kCGImageSourceCreateThumbnailWithTransform;
-    myValuesT[0] = (CFTypeRef) kCFBooleanFalse;
+    myValuesT[0] = (CFTypeRef)kCFBooleanFalse;
     myKeysT[1] = kCGImageSourceCreateThumbnailFromImageIfAbsent;
-    myValuesT[1] = (CFTypeRef) kCFBooleanTrue;
+    myValuesT[1] = (CFTypeRef)kCFBooleanTrue;
     myKeysT[2] = kCGImageSourceThumbnailMaxPixelSize;
-    myValuesT[2] = (CFTypeRef) thumbnailSize;
+    myValuesT[2] = (CFTypeRef)thumbnailSize;
     myKeysT[3] = kCGImageSourceCreateThumbnailFromImageAlways;
-    myValuesT[3] = (CFTypeRef) kCFBooleanTrue;
+    myValuesT[3] = (CFTypeRef)kCFBooleanTrue;
     myKeysT[4] = kCGImageSourceShouldCache;
-    myValuesT[4] = (CFTypeRef) kCFBooleanTrue;
+    myValuesT[4] = (CFTypeRef)kCFBooleanTrue;
 
-    myOptionsT = CFDictionaryCreate(NULL, (const void **) myKeysT, (const void **) myValuesT, 5
-            , &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    myOptionsT = CFDictionaryCreate(NULL, (const void **)myKeysT, (const void **)myValuesT, 5,
+                                    &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
 
     thumbnail = CGImageSourceCreateThumbnailAtIndex(myImageSource, 0, myOptionsT);
 
@@ -126,9 +123,12 @@
     CFRelease(myOptionsT);
     CFRelease(myImageSource);
 
-    if( thumbnail == NULL ){
+    if (thumbnail == NULL) {
+#if wdDebugLoggingEnabled
         NSLog(@"image thumbnail is null");
-        if( aError != NULL ) *aError = [NSError errorWithDomain:WD_ASYNC_IMAGE_ERROR_DOMAIN code:-41 userInfo:nil];
+#endif
+        if (aError != NULL)
+            *aError = [NSError errorWithDomain:WD_ASYNC_IMAGE_ERROR_DOMAIN code:-41 userInfo:nil];
         return NULL;
     }
     [imagePath stopAccessingSecurityScopedResource];
